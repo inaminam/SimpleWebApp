4c125f1beb6ef266ccccf9de2b898b47b1356fba
ape09 <iur@cantab.net>
2019-03-18 15:26:21 +0000

    Added a new query for the word inam

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -7,6 +7,12 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+
+        if (query.toLowerCase().contains("inam")) {
+            return "Inam (01 August 1980 - alive) was an " +
+                    "Not an English poet, playwright, and actor, widely regarded as the greatest " +
+                    "writer in the English language and the world's pre-eminent dramatist.";
+        }
         return "";
     }
 }
